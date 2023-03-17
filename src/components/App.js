b43589bf6380be05ed9168305a98b53b60c48dcd
Artem Morozov <deadtems@yandex.ru>
2023-03-17 21:35:56 +0500

    feat: Сделать портирование + API

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import CurrentUserContext from '../contexts/CurrentUserContext';
-import api from '../utils/api';
+import api from '../utils/Api';
 
 import { Header } from './Header';
 import { Main } from './Main';
